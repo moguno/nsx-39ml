d8a7db8ed11cd52a31fac4873c7a817d36cd4e53
Your Name <you@example.com>
2014-04-06 16:32:45 +0900

    しゃべる様になった

--- make_jihou.rb
+++ make_jihou.rb
@@ -52,30 +52,85 @@ nsx39.conductor {
   tempo("bpm" => 90)
 }
 
+
+def make_notes(lyrics)
+  lyrics.map { |lyric|
+    key = ["C", "E", "G"].sample + ["4"]. sample
+
+    if lyric == "っ"
+      {"key" => "R", "lyric" => lyric, "length" => 4}
+    else
+      {"key" => key, "lyric" => lyric, "length" => 4}
+    end
+  }
+end
+
+
+HOURS = [
+  [ "じゅ", "う", "に" ],
+  [ "い", "ち" ],
+  [ "に" ],
+  [ "さ", "ん" ],
+  [ "よ" ],
+  [ "ご" ],
+  [ "ろ", "く" ],
+  [ "し", "ち" ],
+  [ "は", "ち" ],
+  [ "く" ],
+  [ "じゅ", "う" ],
+  [ "じゅ", "う", "い", "ち" ]
+]
+
+MINUTES_10_JUST = [
+  [],
+  [ "じゅ", "っ" ],
+  [ "に", "じゅ", "っ" ],
+  [ "さ", "ん", "じゅ", "っ" ],
+  [ "よ", "ん", "じゅ", "っ" ],
+  [ "ご", "じゅ", "っ" ],
+]
+
+MINUTES_10_FRAC = [
+  [],
+  [ "じゅ", "う" ],
+  [ "に", "じゅ", "う" ],
+  [ "さ", "ん", "じゅ", "う" ],
+  [ "よ", "ん", "じゅ", "う" ],
+  [ "ご", "じゅ", "う" ],
+]
+
+MINUTES_1 = [
+  [],
+  [ "い", "っ" ],
+  [ "に" ],
+  [ "さ", "ん" ],
+  [ "よ" ,"ん" ],
+  [ "ご" ],
+  [ "ろ", "っ" ],
+  [ "な", "な" ],
+  [ "は", "ち" ],
+  [ "きゅ", "う" ],
+]
+
+
+now = Time.now
+
+lyrics = []
+
+lyrics += HOURS[now.hour % 12] + [ "じ" ]
+
+if now.min == 0
+  # nop
+elsif (now.min % 10) == 0
+  lyrics += MINUTES_10_JUST[now.min / 10] + [ "ぷ", "ん" ]
+else
+  lyrics += MINUTES_10_FRAC[now.min / 10] + MINUTES_1[now.min % 10] + [ "ふ", "ん" ]
+end
+
+lyrics += [ "で", "す" ]
+
 nsx39.channel(1) {
-  ["じゅ", "う", "じ"].each { |lyric|
-    key = ["C", "E", "G"].sample + ["4", "5"]. sample
-
-    note("key" => key, "lyric" => lyric, "length" => 4)
-  }
-
-  ["さ", "ん２", "じゅ", "う"].each { |lyric|
-    key = ["C", "E", "G"].sample + ["4"]. sample
-
-    note("key" => key, "lyric" => lyric, "length" => 4)
-  }
-
-  ["に"].each { |lyric|
-    key = ["C", "E", "G"].sample + ["4"]. sample
-
-    note("key" => key, "lyric" => lyric, "length" => 4)
-  }
-
-  ["ふ", "ん２"].each { |lyric|
-    key = ["C", "E", "G"].sample + ["4"]. sample
-
-    note("key" => key, "lyric" => lyric, "length" => 4)
-  }
+  make_notes(lyrics).each { |_note| note(_note) }
 }
 
 File.open(ARGV[0], "wb") { |file|
